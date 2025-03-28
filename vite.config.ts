6d2a9a76435573aec788b2cbabfa334ab134935d
Eric Wing <wingman@Erics-MacBook-Pro.local>
2025-03-27 22:30:37 -0400

    readd ignore files and change base to simply '/'...debugging

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Replace 'your-github-username' with your actual GitHub username
 export default defineConfig({
   plugins: [react()],
-  base: '/ecwing.github.io/', // Make sure this matches your GitHub repo name
+  base: '/',
 });
