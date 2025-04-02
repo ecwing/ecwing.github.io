feddc25909c1b70b136c1ef6db22d85850c39878
Eric Wing <wingman@Erics-MacBook-Pro.local>
2025-04-01 22:14:47 -0400

    Fix image paths

--- src/components/SkillGrid.tsx
+++ src/components/SkillGrid.tsx
@@ -7,20 +7,20 @@ import {
 } from "../styles/SkillsPage.ts";
 
 const skills = [
-  { name: "HTML", src: "../public/logos/html-logo.png" },
-  { name: "CSS", src: "../public/logos/css-logo.png" },
-  { name: "SASS", src: "../public/logos/sass-logo.png" },
+  { name: "HTML", src: "../../public/logos/html-logo.png" },
+  { name: "CSS", src: "../../public/logos/css-logo.png" },
+  { name: "SASS", src: "../../public/logos/sass-logo.png" },
   {
     name: "styled-components",
-    src: "../public/logos/styled-components-logo.png",
+    src: "../../public/logos/styled-components-logo.png",
   },
-  { name: "JavaScript", src: "../public/logos/js-logo.png" },
+  { name: "JavaScript", src: "../../public/logos/js-logo.png" },
   { name: "TypeScript", src: "/../public/logos/ts-logo.png" },
-  { name: "React", src: "../public/logos/reactjs-logo.png" },
-  { name: "Redux", src: "../public/logos/redux-logo.svg" },
-  { name: "Node.js", src: "../public/logos/nodejs-logo.png" },
-  { name: "MongoDB", src: "../public/logos/mongodb-logo.png" },
-  { name: "Git", src: "../public/logos/git-logo.png" },
+  { name: "React", src: "../../public/logos/reactjs-logo.png" },
+  { name: "Redux", src: "../../public/logos/redux-logo.svg" },
+  { name: "Node.js", src: "../../public/logos/nodejs-logo.png" },
+  { name: "MongoDB", src: "../../public/logos/mongodb-logo.png" },
+  { name: "Git", src: "../../public/logos/git-logo.png" },
 ];
 
 const SkillGrid: React.FC = () => {
